9a8a31f081fe4b0c9cc0f02c39536bfc5d8fe6a8
Yash Thapliyal <yash.thapliyal.007@gmail.com>
2024-03-19 10:46:19 -0600

    Database initialization and GET endpoints for initial pages

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -75,107 +75,32 @@ app.use(
 // *****************************************************
 
 app.get('/', (req, res) => {
-    res.redirect('/login'); 
+    res.redirect('/gallery'); 
 });
 
-app.get('/register', (req, res) => {
-    res.render('pages/register')
+app.get('/gallery', (req, res) => {
+    res.render('pages/gallery')
 });
 
-// Register
-app.post('/register', async (req, res) => {
-  //hash the password using bcrypt library
-  const hash = await bcrypt.hash(req.body.password, 10);
+app.get('/closet', (req, res) => {
+  res.render('pages/closet')
+});
 
-  // To-DO: Insert username and hashed password into the 'users' table
-  const query =
-    'insert into userinfo (username, password) values ($1, $2) returning * ;';
-  db.any(query, [
-      req.body.username,
-      hash,
-    ])
-      // if query execution succeeds
-      // send success message
-      .then(function (data) {
-        res.redirect('/login');
-      })
-      // if query execution fails
-      // send error message
-      .catch(function (err) {
-        res.redirect('/register');
-      });
+app.get('/accountDetails', (req, res) => {
+  res.render('pages/gallery')
 });
 
 app.get('/login', (req, res) => {
-  res.render('pages/login')
+  res.render('pages/gallery')
 });
 
-app.post('/login', async (req, res) => {
-  const username = req.body.username
-  const query = 'SELECT * from userinfo where userinfo.username = $1 LIMIT 1';
-  try {
-    const user = await db.one(query, [username])
-    if (!user) {
-      res.redirect('/register');
-    }
-    const match = await bcrypt.compare(req.body.password, user.password);
-    if (match) {
-      req.session.user = user;
-      req.session.save();
-      res.redirect("/discover")
-    }
-    else {
-      res.redirect("/register")
-    }
-  }
-  catch {
-      res.redirect('/register');
-  }
-});
-
-// Authentication Middleware.
-const auth = (req, res, next) => {
-  if (!req.session.user) {
-    // Default to login page.
-    return res.redirect('/login');
-  }
-  next();
-};
-
-// Authentication Required
-app.use(auth); 
-
-app.get('/discover', (req, res) => {
-  axios({
-    url: `https://app.ticketmaster.com/discovery/v2/events.json`,
-    method: 'GET',
-    dataType: 'json',
-    headers: {
-      'Accept-Encoding': 'application/json',
-    },
-    params: {
-      apikey: process.env.API_KEY,
-      size: 10 // you can choose the number of events you would like to return
-    },
-  })
-    .then(results => {
-      const events = results.data._embedded.events
-      res.render('pages/discover', {
-        events,
-      }) 
-    })
-    .catch(error => {
-      // Handle errors
-    });
-  
+app.get('/register', (req, res) => {
+  res.render('pages/register')
 });
 
 app.get('/logout', (req, res) => {
-  req.session.destroy();
-  res.render('pages/logout');
+  res.render('pages/logout')
 });
-
-
 // *****************************************************
 // <!-- Section 5 : Start Server-->
 // *****************************************************
--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   username VARCHAR(100) PRIMARY KEY,
-  password VARCHAR(100) NOT NULL
+  password VARCHAR(100) NOT NULL,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL
 );
 
 DROP TABLE IF EXISTS items;
 CREATE TABLE items (
-  product_id SERIAL PRIMARY KEY,
+  item_id SERIAL PRIMARY KEY,
   name VARCHAR(100) UNIQUE NOT NULL,
   price SMALLINT,
   image_url VARCHAR(200) NOT NULL,
@@ -34,30 +34,28 @@ CREATE TABLE outfits (
   brand VARCHAR(50)
 );
 
-DROP TABLE IF EXISTS prerequisites;
-CREATE TABLE IF NOT EXISTS prerequisites (
-  course_id INTEGER NOT NULL REFERENCES courses (course_id),
-  prerequisite_id INTEGER NOT NULL REFERENCES courses (course_id)
+DROP TABLE IF EXISTS users_to_items;
+CREATE TABLE IF NOT EXISTS users_to_items (
+  item_id INTEGER NOT NULL REFERENCES items (item_id),
+  category_id INTEGER NOT NULL REFERENCES categories (category_id)
 );
 
--- Views to simplify queries in the server.
+DROP TABLE IF EXISTS users_to_outfits;
+CREATE TABLE IF NOT EXISTS users_to_outfits (
+  username VARCHAR(100) NOT NULL REFERENCES users (username),
+  outfit_id INTEGER NOT NULL REFERENCES outfits (outfit_id)
+);
 
-CREATE OR REPLACE VIEW course_prerequisite_count AS
-  SELECT
-    c.course_id,
-    COUNT(p.prerequisite_id) AS num_prerequisites
-  FROM
-    courses AS c
-    LEFT JOIN prerequisites AS p ON c.course_id = p.course_id
-  GROUP BY c.course_id;
+DROP TABLE IF EXISTS items_to_outfits;
+CREATE TABLE IF NOT EXISTS items_to_outfits (
+  item_id INTEGER NOT NULL REFERENCES items (item_id),
+  outfit_id INTEGER NOT NULL REFERENCES outfits (outfit_id)
+);
 
-CREATE OR REPLACE VIEW student_prerequisite_count AS
-  SELECT
-    c.course_id,
-    sc.student_id,
-    COUNT(*) AS num_prerequisites_satisfied
-  FROM
-    courses AS c
-    LEFT JOIN prerequisites AS p ON c.course_id = p.course_id
-    LEFT JOIN student_courses AS sc ON p.prerequisite_id = sc.course_id
-  GROUP BY c.course_id, sc.student_id;
+DROP TABLE IF EXISTS items_to_categories;
+CREATE TABLE IF NOT EXISTS items_to_categories (
+  item_id INTEGER NOT NULL REFERENCES items (item_id),
+  category_id INTEGER NOT NULL REFERENCES categories (category_id)
+);
+
+\dt;
