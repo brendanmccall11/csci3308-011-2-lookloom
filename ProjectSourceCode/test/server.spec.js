dd36c50a44f8398fa56d8f82ff1e1924ea57992e
ThanhThanhDao <thda7632@colorado.edu>
2024-04-03 13:42:17 -0600

    negative test case for lab 11

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -1,27 +1,27 @@
 // ********************** Initialize server **********************************
 
-const server = require('../index'); //TODO: Make sure the path to your index.js is correctly added
+const server = require("../index"); //TODO: Make sure the path to your index.js is correctly added
 
 // ********************** Import Libraries ***********************************
 
-const chai = require('chai'); // Chai HTTP provides an interface for live integration testing of the API's.
-const chaiHttp = require('chai-http');
+const chai = require("chai"); // Chai HTTP provides an interface for live integration testing of the API's.
+const chaiHttp = require("chai-http");
 chai.should();
 chai.use(chaiHttp);
-const {assert, expect} = chai;
+const { assert, expect } = chai;
 
 // ********************** DEFAULT WELCOME TESTCASE ****************************
 
-describe('Server!', () => {
+describe("Server!", () => {
   // Sample test case given to test / endpoint.
-  it('Returns the default welcome message', done => {
+  it("Returns the default welcome message", (done) => {
     chai
       .request(server)
-      .get('/welcome')
+      .get("/welcome")
       .end((err, res) => {
         expect(res).to.have.status(200);
-        expect(res.body.status).to.equals('success');
-        assert.strictEqual(res.body.message, 'Welcome!');
+        expect(res.body.status).to.equals("success");
+        assert.strictEqual(res.body.message, "Welcome!");
         done();
       });
   });
@@ -36,19 +36,58 @@ describe('Server!', () => {
 // Explanation: The testcase will call the /add_user API with the following input
 // and expects the API to return a status of 200 along with the "Success" message.
 
-describe('Testing Add User API', () => {
-  it('positive : /register', done => {
+describe("Testing Add User API", () => {
+  it("positive : /register", (done) => {
     chai
       .request(server)
-      .post('/register')
-      .send({username: 'MyWife', password: 'gangang', firstName: 'Your', lastName: 'Mother'})
+      .post("/register")
+      .send({
+        username: "MyWife",
+        password: "gangang",
+        firstName: "Your",
+        lastName: "Mother",
+      })
       .end((err, res) => {
         expect(res).to.have.status(200);
-        expect(res.body.message).to.equals('Success');
+        expect(res.body.message).to.equals("Success");
         done();
       });
   });
 });
 
+describe("Testing Add User API", () => {
+  it("positive : /register", (done) => {
+    chai
+      .request(server)
+      .post("/register")
+      .send({
+        username: "MyWife",
+        password: "gangang",
+        firstName: "Your",
+        lastName: "Mother",
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equals("Success");
+        done();
+      });
+  });
+  it("negative : /register", (done) => {
+    chai
+      .request(server)
+      .post("/register")
+      .send({
+        username: "Johndoey",
+        password: "i_am_doe",
+        firstName: "John",
+        lastName: "Doe",
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(400); // Expecting a 400 Bad Request status code
+        expect(res.body.message).to.equals("Invalid username or password"); // Expecting an appropriate error message
+        done();
+      });
+  });
+});
 
 // ********************************************************************************
