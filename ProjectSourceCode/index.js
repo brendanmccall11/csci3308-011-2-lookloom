f412454fff1cb39010fd170b1f89f9ea57420b96
Brendan McCall <brendanmccall3@gmail.com>
2024-04-04 04:20:50 -0600

    Added Gallery Page

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -54,6 +54,7 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
+app.use(express.static('public'))
 
 // initialize session variables
 app.use(
